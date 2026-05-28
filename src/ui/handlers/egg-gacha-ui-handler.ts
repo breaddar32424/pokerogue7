3073f800dbf9e8488d0961d7f61f22275c1c3046
breaddar32424 <braydenlim710@gmail.com>
2026-05-28 15:49:34 +0800

    Update egg-gacha-ui-handler.ts

--- src/ui/handlers/egg-gacha-ui-handler.ts
+++ src/ui/handlers/egg-gacha-ui-handler.ts
@@ -757,7 +757,7 @@ export class EggGachaUiHandler extends MessageUiHandler {
     let errorKey: string | undefined;
     const freePulls = activeOverrides.EGG_FREE_GACHA_PULLS_OVERRIDE;
 
-    if (!freePulls && globalScene.gameData.eggs.length + pulls > 99) {
+    if (!freePulls && globalScene.gameData.eggs.length + pulls > 9999) {
       errorKey = "egg:tooManyEggs";
     } else if (!freePulls && globalScene.gameData.voucherCounts[voucherType] < vouchersConsumed) {
       errorKey = "egg:notEnoughVouchers";
